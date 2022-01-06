b5cf6361197a99d0c24084ffb0cde3e3ee144602
André Leite <adeoliveiraleite@ebay.com>
2022-01-06 15:42:15 -0300

    First commit

---
+++ src/test/java/io/andre/AppTest.java
@@ -0,0 +1,12 @@
+package io.andre;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class AppTest {
+  @Test
+  public void shouldAnswerWithTrue() {
+    assertTrue(true);
+  }
+}
