278c150eacd68ff35930f4d3a6dc54e39a2effe6
André Leite <adeoliveiraleite@ebay.com>
2022-01-06 19:28:54 -0300

    Add leetCode0001TwoSum

--- src/main/java/io/andre/App.java
+++ src/main/java/io/andre/App.java
@@ -1,7 +1,18 @@
 package io.andre;
 
+import java.util.HashMap;
+
 public class App {
-  public static void main(String[] args) {
-    System.out.println("Hello World!");
+  public static int[] leetCode0001TwoSum(int[] nums, int target) {
+    int len = nums.length;
+    HashMap<Integer, Integer> map = new HashMap<>();
+    for (int i = 0; i < len; ++i) {
+      final Integer value = map.get(nums[i]);
+      if (value != null) {
+        return new int[] {value, i};
+      }
+      map.put(target - nums[i], i);
+    }
+    return null;
   }
 }
--- src/test/java/io/andre/AppTest.java
+++ src/test/java/io/andre/AppTest.java
@@ -1,12 +1,34 @@
 package io.andre;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertArrayEquals;
 
 import org.junit.Test;
 
 public class AppTest {
   @Test
-  public void shouldAnswerWithTrue() {
-    assertTrue(true);
+  public void testLeetCode0001TwoSum() {
+    int[] expected1 = new int[] {0, 1};
+    int[] nums1 = new int[] {2, 7, 11, 15};
+    int target1 = 9;
+
+    int[] actual1 = App.leetCode0001TwoSum(nums1, target1);
+
+    assertArrayEquals(expected1, actual1);
+
+    int[] expected2 = new int[] {1, 2};
+    int[] nums2 = new int[] {3, 2, 4};
+    int target2 = 6;
+
+    int[] actual2 = App.leetCode0001TwoSum(nums2, target2);
+
+    assertArrayEquals(expected2, actual2);
+
+    int[] expected3 = new int[] {0, 1};
+    int[] nums3 = new int[] {3, 3};
+    int target3 = 6;
+
+    int[] actual3 = App.leetCode0001TwoSum(nums3, target3);
+
+    assertArrayEquals(expected3, actual3);
   }
 }
